b2232af143422db76dfd8c0120f6afffcc5e6324
霸气千秋 <zhangshaomin_1990@126.com>
2019-01-20 01:33:28 +0800

    Update composer.sh

--- composer.sh
+++ composer.sh
@@ -1,8 +1,8 @@
 # 修改全局配置
-composer config -g repo.packagist composer https://packagist.phpcomposer.com
+composer config -g repo.packagist composer https://packagist.laravel-china.org
 
 # 修改项目配置
-# composer config repo.packagist composer https://packagist.phpcomposer.com
+# composer config repo.packagist composer https://packagist.laravel-china.org
 
 # 清除缓存
 composer clear
